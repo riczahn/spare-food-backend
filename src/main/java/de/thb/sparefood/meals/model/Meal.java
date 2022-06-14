8adbcfba5ba67f981a12a39d288f39775f30af1d
Richard <Richard-Zahn@web.de>
2022-06-14 20:01:37 +0200

    add basic support for MealProperties

--- src/main/java/de/thb/sparefood/meals/model/Meal.java
+++ src/main/java/de/thb/sparefood/meals/model/Meal.java
@@ -2,11 +2,12 @@ package de.thb.sparefood.meals.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import de.thb.sparefood.user.model.User;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "meal")
@@ -17,22 +18,31 @@ public class Meal {
   private String name;
   private String description;
 
+  @Column
+  @Enumerated
+  @ElementCollection(targetClass = Property.class, fetch = FetchType.EAGER)
+  private List<Property> properties;
+
   @JsonIgnore
   @ManyToOne(optional = false, fetch = FetchType.EAGER)
   private User creator;
 
   public Meal(String name) {
-    this.name = name;
+    this(name, null);
   }
 
   public Meal(String name, User user) {
-    this.name = name;
-    this.creator = user;
+    this(name, null, user);
   }
 
   public Meal(String name, String description, User user) {
+    this(name, description, new ArrayList<>(), user);
+  }
+
+  public Meal(String name, String description, List<Property> properties, User user) {
     this.name = name;
     this.description = description;
+    this.properties = properties;
     this.creator = user;
   }
 
---
+++ src/main/java/de/thb/sparefood/meals/model/Property.java
@@ -0,0 +1,5 @@
+package de.thb.sparefood.meals.model;
+
+public enum Property {
+  VEGETARIAN, VEGAN, CONTAINS_FISH
+}
