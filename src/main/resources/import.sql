56be6e013dcb5d313db4a29e2a5cec9702fb368c
Richard <Richard-Zahn@web.de>
2022-07-05 18:01:45 +0200

    update import script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,19 @@ VALUES ('testuser@test.de', 'Test', 'Testuser', 'password'),
        ('trompell@th-brandenburg.de', 'Tobias', 'Trompell', 'password'),
        ('luedrick@th-brandenburg.de', 'Rick', 'Lüdicke', 'password'),
        ('zahn@th-brandenburg.de', 'Richard', 'Zahn', 'password');
+
+INSERT INTO meal(id, name, description, creator_email)
+VALUES (1, 'Dönerreste', 'Ein leckerer Döner', 'zahn@th-brandenburg.de');
+INSERT INTO meal(id, name, description, creator_email)
+VALUES (2, 'Vegetarische Pasta', 'Nur Biozutaten', 'zahn@th-brandenburg.de');
+INSERT INTO meal(id, name, description, creator_email, reservinguser_email)
+VALUES (3, 'Hamburger', 'This meal is reserved by default', 'zahn@th-brandenburg.de', 'zahn@th-brandenburg.de');
+
+INSERT INTO meal_properties(meal_id, properties)
+VALUES (1, 'NO_FISH');
+INSERT INTO meal_properties(meal_id, properties)
+VALUES (2, 'VEGETARIAN');
+INSERT INTO meal_properties(meal_id, properties)
+VALUES (2, 'NUTS');
+INSERT INTO meal_properties(meal_id, properties)
+VALUES (3, 'GLUTEN_FREE');
